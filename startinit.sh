2d95d6641e77bebc4796a7bcc188c29ab1398533
suisrc <suisrc@outlook.com>
2022-08-31 19:08:54 +0800

    set workers=3

--- startinit.sh
+++ startinit.sh
@@ -8,7 +8,8 @@ SECRET_KEY=`python3 /srv/webvirtcloud/conf/runit/secret_generator.py`
 ## 处理配置
 cd /srv/webvirtcloud
 cp webvirtcloud/settings.py.template webvirtcloud/settings.py
-sed -r "s/SECRET_KEY = \"\"/SECRET_KEY = \"${SECRET_KEY}\"/" -i webvirtcloud/settings.py
+sed -e "s/SECRET_KEY = \"\"/SECRET_KEY = \"${SECRET_KEY}\"/" -i webvirtcloud/settings.py
+sed -e "s/workers = get_workers()/workers = 3/" -i gunicorn.conf.py
 
 ## 应用配置
 . venv/bin/activate && \
